92e15800646e775639f86046183c75026f07cc4b
m1nd <m1nd@mail.ru>
2017-05-30 22:41:30 +0300

    Route grouping by user, add protection

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var csrf = require('csurf');
-var passport = require('passport');
-var csrfProtection = csrf();
 
 var Product = require('../models/product');
-
-router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,37 +11,8 @@ router.get('/', function(req, res, next) {
       for (let i= 0; i < docs.length; i += chunkSize) {
         productChunks.push(docs.slice(i, i + chunkSize));
       }
-      res.render('shop/index', { title: 'Shopping cart', products: productChunks });
-      
+      res.render('shop/index', { title: 'Shopping cart', products: productChunks });      
   });
-
 });
-
-router.get('/user/signup', function (req, res, next) {
-  var messages = req.flash('error');
-  res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 })
-});
-
-router.post('/user/signup', passport.authenticate('local.signup', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signup',
-  failureFlash: true
-}));
-
-router.get('/user/profile', function (req, res, next) {
-  res.render('user/profile');
-});
-
-router.get('/user/signin', function (req, res, next) {
-  var messages = req.flash('error');
-  res.render('user/signin', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 })
-});
-
-router.post('/user/signin', passport.authenticate('local.signin', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signin',
-  failureFlash: true
-}));
-
 
 module.exports = router;
